0ff25abdd173632389bd006738eb083ea7651e28
vasilecampeanu <vasilecampeanu.link@yahoo.com>
2022-07-12 14:32:36 +0300

    feat[plugins]: Add relevant plugins for code completion.

--- lua/plugins/packer.lua
+++ lua/plugins/packer.lua
@@ -33,10 +33,27 @@ packer.init({
 
 -- Plugin list
 return packer.startup(function(use)
+    -- packer.nvim
+    -- Package manager
+    -- Github link: https://github.com/wbthomason/packer.nvim.git
 	use({ "wbthomason/packer.nvim", commit = "e4c2afb37d31e99b399425e102c58b091fbc16be" })
 
-	-- Automatically set up your configuration after cloning packer.nvim
-	-- Put this at the end after all plugins
+    -- Language server protocol
+    use({ "neovim/nvim-lspconfig", commit = "41a826947eefbc7b59cc80cef405cce9ea6d82b8" })
+
+    -- Code completion plugins
+
+    -- nvim-cmp
+    -- A completion plugin for neovim coded in Lua.
+    -- This plugin is separated in multiple modules.
+    -- Github link: https://github.com/hrsh7th/nvim-cmp.git
+    use({ "hrsh7th/nvim-cmp", commit = "9897465a7663997b7b42372164ffc3635321a2fe" })
+    use({ "hrsh7th/cmp-nvim-lsp", commit = "affe808a5c56b71630f17aa7c38e15c59fd648a8" })
+    use({ "hrsh7th/cmp-buffer", commit = "62fc67a2b0205136bc3e312664624ba2ab4a9323" })
+    use({ "hrsh7th/cmp-path", commit = "981baf9525257ac3269e1b6701e376d6fbff6921" })
+
+	-- Automatically set up your configuration after cloning packer.nvim.
+	-- Put this at the end after all plugins.
 	if PACKER_BOOTSTRAP then
 		require("packer").sync()
 	end
