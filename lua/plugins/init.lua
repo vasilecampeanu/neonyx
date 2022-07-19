700992bc480be8bcf1cf2059a4d31f97e62fd238
vasilecampeanu <vasilecampeanu.link@yahoo.com>
2022-07-19 17:40:35 +0300

    feat: Add mapping for git sign hunks.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -41,6 +41,8 @@ local plugins = {
         module = "nvim-web-devicons"
     },
 
+    -- Test
+
     ---
 
     -- Onedarker colorscheme
@@ -154,7 +156,17 @@ local plugins = {
                         numhl = "GitSignsChangeNr",
                         linehl = "GitSignsChangeLn"
                     }
-                }
+                },
+                on_attach = function(bufnr)
+                    local function map(mode, lhs, rhs, opts)
+                        opts = vim.tbl_extend('force', {noremap = true, silent = true}, opts or {})
+                        vim.api.nvim_buf_set_keymap(bufnr, mode, lhs, rhs, opts)
+                    end
+
+                    -- Hotkeys 
+                    map('n', ']c', "&diff ? ']c' : '<cmd>Gitsigns next_hunk<CR>'", {expr = true})
+                    map('n', '[c', "&diff ? '[c' : '<cmd>Gitsigns prev_hunk<CR>'", {expr = true})
+                end
             })
         end
     },
